5c387ab488d8eda92b83fa5e84a84c7a26e51734
Gurkirat Singh <gurkiratsinghh0001@gmail.com>
2026-03-04 15:23:42 +0530

    Use prompt loader for templates in making a summarize ans chatbot

---
+++ LangChain_Prompts/3_prompt_loader.py
@@ -0,0 +1,47 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+endpoint = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="conversational"
+)
+
+# Step 2: Wrap into chat model
+chat = ChatHuggingFace(llm=endpoint)
+st.header("Research Tool")
+
+paper_input = st.selectbox(
+    "Select Research Paper Name",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis"
+    ]
+)
+
+style_input = st.selectbox(
+    "Select Explanation Style",
+    ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"]
+)
+
+length_input = st.selectbox(
+    "Select Explanation Length",
+    ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"]
+)
+
+#template
+template=load_prompt("LangChain_Prompts/template.json")
+#fill the plceholder
+prompt = template.invoke({
+    "paper_input": paper_input,
+    "style_input": style_input,
+    "length_input": length_input
+})
+
+if st.button("Summarize"):
+    result=chat.invoke(prompt)
+    st.write(result.content)
